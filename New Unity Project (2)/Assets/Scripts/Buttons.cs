9a87283681e206f4b270bf7bb966e36fe9016f9b
laurence <l.peat@saedubai.com>
2018-01-26 15:48:07 +0400

    yeet

---
+++ New Unity Project (2)/Assets/Scripts/Buttons.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public int buttNum;
+
+
+    public void OnCollisionEnter(Collision col)
+    {
+       if (col.gameObject.tag == ("Hand"))
+        {
+            GameObject.FindObjectOfType<NewManager>().Verify(buttNum);
+        } 
+    }
+
+
+
+
+}
---
+++ New Unity Project (2)/Assets/Scripts/NewManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewManager : MonoBehaviour {
+
+    public int[] buttOrder;
+
+    int i = 0;
+
+
+    public void Verify(int buttNum)
+    {
+        if (buttNum == buttOrder[i])
+        {
+            i++;
+            if (i == buttOrder.Length)
+            {
+                Debug.Log("game won");
+            } 
+        }
+        else
+        {
+            Debug.Log("lost");
+        }
+    }
+	
+	
+}
