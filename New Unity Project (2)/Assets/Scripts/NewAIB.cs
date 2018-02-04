345be3bf66f814dbe4647d216c7d9ad26867df43
Sasan <s.faizollahzadeh@saedubai.com>
2018-02-04 17:23:22 +0400

    changes

--- New Unity Project (2)/Assets/Scripts/NewAIB.cs
+++ New Unity Project (2)/Assets/Scripts/NewAIB.cs
@@ -5,31 +5,38 @@ using UnityEngine.AI;
 
 public class NewAIB : MonoBehaviour {
 
-    public Transform [] targets;
+    public Transform[] targets;
     [SerializeField]
     private Transform currtarget;
     NavMeshAgent agent;
-
-	// Use this for initialization
-	void Start ()
+    GameObject cube;
+    // Use this for initialization
+    void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         currtarget = targets[6];
-        agent.SetDestination(currtarget.position); 
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        if (agent.transform.position != currtarget.position)
-        {
-            agent.SetDestination(currtarget.position);
-        }
-       // agent.SetDestination(target.position);
-	}
-
-    public void newpos(int nodePos)
-    {
-        currtarget = targets[nodePos];
+        agent.SetDestination(currtarget.position);
+        cube = GameObject.Find("MyCube");
     }
-}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //AgentMoveToNode();
+        agent.SetDestination(cube.transform.position);
+    }
+
+        private void AgentMoveToNode()
+        {
+            if (agent.transform.position != currtarget.position)
+            {
+                agent.SetDestination(currtarget.position);
+            }
+        }
+
+        public void newpos(int nodePos)
+        {
+            currtarget = targets[nodePos];
+        }
+    }
+
